2399840c0747ad764434e0154da28450798c82d9
dudesdog <64710010+dudesdog@users.noreply.github.com>
2021-04-06 16:19:16 -0500

    Update authentication.js

--- src/login-page/authentication.js
+++ src/login-page/authentication.js
@@ -1,22 +1,9 @@
 function handleAuth() {
-
-  //firebase sign up code from their website.. 
-  /*firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ..
-  });
- */
- 
+  
    // retrieve email/password data
   let email = document.getElementById("email").nodeValue;
   let password = document.getElementById("password").nodeValue;
+
 
   document.getElementById("sign-in-toggle").innerText.trim() !== "Log in" ?
     this.handleLogIn(email, password) :
@@ -24,13 +11,18 @@ function handleAuth() {
 }
 
 function handleLogIn(email, password) {
-
-
-}/*
+  firebase
+  .auth()
+  .signInWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    // TODO: alert the user to these properly
+    console.log(error.code);
+    console.log(error.message);
+  });
+}
 
 function handleSignUp(email, password) {
   //check for confirmed password match
-  if (password === document.getElementById("confirm-password").value) {
     // check that email and password is long enough
     // TODO: make good requirements for eamil/password
     if (email.length < 4) {
@@ -43,4 +35,15 @@ function handleSignUp(email, password) {
 
     // at this point email and password should be approved
   }
-}
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      //user is logged in
+      var uid = user.uid;
+      // otherwise, user is logged out
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
+
